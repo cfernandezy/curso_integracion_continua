285629128659c82cd7e4728be69ffddc047690ba
cfernandezy <56826211+cfernandezy@users.noreply.github.com>
2019-10-22 11:51:25 +0200

    4

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,9 +17,6 @@ public class Greeter {
    */
   public String greet(String someone) {
     Long lalala = new Long("2333");
-    if(true){
-      return String.format("Hello, %s!", someone);
-    }
+    if(true){ return String.format("Hello, %s!", someone);}
     else{return String.format("Hello, %s!", someone);}
-  }
 }
